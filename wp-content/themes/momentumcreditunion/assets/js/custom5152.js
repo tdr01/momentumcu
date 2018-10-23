da5f67c0176c1eb98d4a062897880d487a93b7be
hcorzo22 <44207002+hcorzo22@users.noreply.github.com>
2018-10-23 13:53:24 -0400

    Add files via upload

---
+++ wp-content/themes/momentumcreditunion/assets/js/custom5152.js
@@ -0,0 +1,21 @@
+/* global window, console */
+/* jslint devel: true */
+console.log("%c Momentum Credit Union ", "font-size: 15px; color:#000000;");
+console.log("%c Coding by:", "font-size:15px; color: #000000;");
+console.log("%c Matei Tudor   •   studio@mateitudor.com", "font-size:15px; color: #000000;");
+console.log(" ");
+console.log("%c Make the world a better place. Donate any tiny bit to:", "font-size: 15px; background: #6db33f; color: #000000;");
+console.log("%c Mercy For Animals   •   https://www.mercyforanimals.org/donate", "font-size: 15px; background: #6db33f; color: #000000;");
+
+// Calculate the dimensions of the window
+// Used for dev purposes only
+// function displayWindowSize() {
+//   var myWidth;
+//   var myHeight;
+//   myWidth = window.innerWidth;
+//   myHeight = window.innerHeight;
+//   document.getElementById("debug_windowsize").innerHTML = myWidth + "x" + myHeight;
+// }
+// window.onresize = displayWindowSize;
+// window.onload = displayWindowSize;
+
